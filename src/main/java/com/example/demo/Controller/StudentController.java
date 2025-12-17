ad3bbb290a967b996c2c5866ac565042a519ff4e
bala-amypo <bala@amypo.in>
2025-12-17 06:48:15 +0000

    User commit

--- src/main/java/com/example/demo/Controller/StudentController.java
+++ src/main/java/com/example/demo/Controller/StudentController.java
@@ -1,1 +1,30 @@
-public class StudentCont
+public class StudentController {
+
+@Autowired
+StudentService studentService;
+
+    @PostMapping("/PostStudent")
+    public Student PostStd(@RequestBody Student st){
+        return studentService.insertStudent(st);
+    }
+    @GetMapping("/getall")
+    public List<Student> getAll()
+    {
+        return studentService.getAllStudents();
+    }
+    @GetMapping("/get/{id}")
+    public Optional<Student> get(@PathVariable int id)
+    {
+        return ser.getbyid(id);
+    }
+    @PutMapping("/update/{id}")
+    public Student update(@PathVariable int id, @RequestBody Student st)
+    {
+        return ser.updateTable(id,st);
+        
+    }
+    @DeleteMapping("/delete/{id}")
+    public Student Delete(@PathVariable int id){
+        return ser.deleteID(id);
+    }
+}
